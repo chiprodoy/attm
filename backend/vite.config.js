83d8a74b960f53fab32fb64bf2ab239a3de524bd
chiprodoy <chiprodoy@gmail.com>
2025-12-14 21:56:51 +0700

    perbaikan arsitektur

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        outDir: '/var/www/attm/build',
+        emptyOutDir: true,
+        manifest: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
