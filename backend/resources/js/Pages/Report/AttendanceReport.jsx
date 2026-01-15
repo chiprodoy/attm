68dd615cd16466b391e71601e6fd6c6cf158d17c
chiprodoy <chiprodoy@gmail.com>
2026-01-15 08:59:51 +0700

    perbaikan tombol cetak laporan

--- backend/resources/js/Pages/Report/AttendanceReport.jsx
+++ backend/resources/js/Pages/Report/AttendanceReport.jsx
@@ -112,6 +112,13 @@ export default function AttendanceReport({auth}) {
                         className="input"
                     />
                 </div>
+                <button
+                    onClick={handlePrint}
+                    className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700"
+                    >
+                    Cetak
+                </button>
+
             </div>
 
             {/* TABLE */}
